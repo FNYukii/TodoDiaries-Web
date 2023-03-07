883033feab93ba843defb17352218d17c536ffe8
Yu357 <forestgreen14689@gmail.com>
2023-03-07 21:29:57 +0900

    feat: Top画面とNotFound画面のルーティングを設定

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFoundScreen from './screens/NotFoundScreen';
+import TopScreen from './screens/TopScreen';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const router = createBrowserRouter([
+	{ path: "/", element: <TopScreen />, errorElement: <NotFoundScreen /> },
+]);
+
 root.render(
   <React.StrictMode>
-    <h1>Todo Diaries</h1>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/screens/NotFoundScreen.tsx
@@ -0,0 +1,11 @@
+function NotFoundScreen() {
+
+	return (
+		<div>
+			<h1>NotFound</h1>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non sapiente id architecto reiciendis. Porro, enim laborum! Veritatis, recusandae magni? Temporibus.</p>
+		</div>
+	)
+}
+
+export default NotFoundScreen
