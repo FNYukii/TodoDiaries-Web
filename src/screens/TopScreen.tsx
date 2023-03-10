d74ed47d4edda447151cfd4196273c818bc171b5
Yu357 <forestgreen14689@gmail.com>
2023-03-10 20:54:53 +0900

    feat: マージン設定

--- src/screens/TopScreen.tsx
+++ src/screens/TopScreen.tsx
@@ -7,7 +7,7 @@ function TopScreen() {
 
 			<Header />
 
-			<main className="w-full mx-auto px-4 lg:width-lg lg:px-0">
+			<main className="mt-4 w-full mx-auto px-4 lg:width-lg lg:px-0">
 
 				<div className="flex justify-between gap-x-4">
 
