5f4ca96c4e69f94f7f1ae00ab2fa3a80266fe65d
Yu357 <forestgreen14689@gmail.com>
2023-03-10 20:54:22 +0900

    feat: スタイル設定

--- src/screens/TopScreen.tsx
+++ src/screens/TopScreen.tsx
@@ -5,25 +5,49 @@ function TopScreen() {
 	return (
 		<div className="bg-gray-100 h-screen">
 
-			<Header/>
+			<Header />
 
 			<main className="w-full mx-auto px-4 lg:width-lg lg:px-0">
 
 				<div className="flex justify-between gap-x-4">
 
 					<div>
-						<h2 className="text-2xl">Todo</h2>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum distinctio facilis atque id sed ipsa, molestiae libero consequuntur minus!</p>
+
+						<h2 className="text-2xl font-bold">Todo</h2>
+
+						<div className="mt-4 bg-white p-2 rounded-xl">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum distinctio facilis atque id sed ipsa, molestiae libero consequuntur minus!</p>
+						</div>
+
+						<div className="mt-4 bg-white p-2 rounded-xl">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum distinctio facilis atque id sed ipsa, molestiae libero consequuntur minus!</p>
+						</div>
 					</div>
 
 					<div>
-						<h2 className="text-2xl">達成履歴</h2>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum distinctio facilis atque id sed ipsa, molestiae libero consequuntur minus!</p>
+
+						<h2 className="text-2xl font-bold">達成履歴</h2>
+
+						<div className="mt-4 bg-white p-2 rounded-xl">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum distinctio facilis atque id sed ipsa, molestiae libero consequuntur minus!</p>
+						</div>
+
+						<div className="mt-4 bg-white p-2 rounded-xl">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum distinctio facilis atque id sed ipsa, molestiae libero consequuntur minus!</p>
+						</div>
 					</div>
 
 					<div>
-						<h2 className="text-2xl">統計</h2>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum distinctio facilis atque id sed ipsa, molestiae libero consequuntur minus!</p>
+
+						<h2 className="text-2xl font-bold">統計</h2>
+
+						<div className="mt-4 bg-white p-2 rounded-xl">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum distinctio facilis atque id sed ipsa, molestiae libero consequuntur minus!</p>
+						</div>
+
+						<div className="mt-4 bg-white p-2 rounded-xl">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum distinctio facilis atque id sed ipsa, molestiae libero consequuntur minus!</p>
+						</div>
 					</div>
 				</div>
 			</main>
