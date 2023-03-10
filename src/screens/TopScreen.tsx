2a72be989d03a878f32ff0565d39af3a63829d50
Yu357 <forestgreen14689@gmail.com>
2023-03-10 20:47:04 +0900

    feat: 3カラムレイアウトを実装

--- src/screens/TopScreen.tsx
+++ src/screens/TopScreen.tsx
@@ -7,10 +7,25 @@ function TopScreen() {
 
 			<Header/>
 
-			<main>
+			<main className="w-full mx-auto px-4 lg:width-lg lg:px-0">
 
-				<h1>Top</h1>
-				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non sapiente id architecto reiciendis. Porro, enim laborum! Veritatis, recusandae magni? Temporibus.</p>
+				<div className="flex justify-between gap-x-4">
+
+					<div>
+						<h2 className="text-2xl">Todo</h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum distinctio facilis atque id sed ipsa, molestiae libero consequuntur minus!</p>
+					</div>
+
+					<div>
+						<h2 className="text-2xl">達成履歴</h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum distinctio facilis atque id sed ipsa, molestiae libero consequuntur minus!</p>
+					</div>
+
+					<div>
+						<h2 className="text-2xl">統計</h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum distinctio facilis atque id sed ipsa, molestiae libero consequuntur minus!</p>
+					</div>
+				</div>
 			</main>
 		</div>
 	)
