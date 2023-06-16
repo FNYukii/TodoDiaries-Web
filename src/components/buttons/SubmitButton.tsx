82586cc65e51ea69d8ba9b6843896e1396069518
Yu357 <forestgreen14689@gmail.com>
2023-06-16 20:19:25 +0900

    clean: コード整形

--- src/components/buttons/SubmitButton.tsx
+++ src/components/buttons/SubmitButton.tsx
@@ -1,20 +1,20 @@
 
 function SubmitButton(props: { text: string, isLoading: boolean, disabled: boolean }) {
 
-    return (
-        <div>
+	return (
+		<div>
 
-            {!props.isLoading &&
-                <button type="submit" disabled={props.disabled} className={`font-bold py-2 px-3 rounded-full transition hover:bg-zinc-100 dark:hover:bg-zinc-900 disabled:text-gray-400 disabled:dark:text-gray-600 disabled:hover:bg-transparent disabled:dark:hover:bg-transparent`}>
-                    {props.text}
-                </button>
-            }
+			{!props.isLoading &&
+				<button type="submit" disabled={props.disabled} className={`font-bold py-2 px-3 rounded-full transition hover:bg-zinc-100 dark:hover:bg-zinc-900 disabled:text-gray-400 disabled:dark:text-gray-600 disabled:hover:bg-transparent disabled:dark:hover:bg-transparent`}>
+					{props.text}
+				</button>
+			}
 
-            {props.isLoading &&
-                <p>Loading ...</p>
-            }
-        </div>
-    )
+			{props.isLoading &&
+				<p>Loading ...</p>
+			}
+		</div>
+	)
 }
 
 export default SubmitButton
