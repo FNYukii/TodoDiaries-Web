7e94f4d67b4a39b4fe24e53528de8e6342e31735
Yu357 <forestgreen14689@gmail.com>
2023-06-16 20:24:49 +0900

    feat: SubmitButtonにローディング画像を表示

--- src/components/buttons/SubmitButton.tsx
+++ src/components/buttons/SubmitButton.tsx
@@ -1,3 +1,4 @@
+import ReactLoading from "react-loading";
 
 function SubmitButton(props: { text: string, isLoading: boolean, disabled: boolean }) {
 
@@ -5,13 +6,19 @@ function SubmitButton(props: { text: string, isLoading: boolean, disabled: boole
 		<div>
 
 			{!props.isLoading &&
-				<button type="submit" disabled={props.disabled} className={`font-bold py-2 px-3 rounded-full transition hover:bg-zinc-100 dark:hover:bg-zinc-900 disabled:text-gray-400 disabled:dark:text-gray-600 disabled:hover:bg-transparent disabled:dark:hover:bg-transparent`}>
+				<button type="submit" disabled={props.disabled} className={`font-bold py-2 px-2 rounded-full transition hover:bg-zinc-100 dark:hover:bg-zinc-900 disabled:text-gray-400 disabled:dark:text-gray-600 disabled:hover:bg-transparent disabled:dark:hover:bg-transparent`}>
 					{props.text}
 				</button>
 			}
 
 			{props.isLoading &&
-				<p>Loading ...</p>
+				<ReactLoading
+					type="spin"
+					color="#333"
+					height="20px"
+					width="20px"
+					className="mx-2"
+				/>
 			}
 		</div>
 	)
