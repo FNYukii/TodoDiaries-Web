dc44e5fec4590c837503a3976d72d9485fc46e1f
Yu357 <forestgreen14689@gmail.com>
2023-06-18 13:01:13 +0900

    feat: サインアウト後に/まで遷移するようにした

--- src/views/screens/AccountScreen.tsx
+++ src/views/screens/AccountScreen.tsx
@@ -1,7 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import AuthService from "../../utilities/AuthService";
 import Modal from "../components/others/Modal";
 
 function AccountScreen() {
+
+	const navigate = useNavigate()
+
+	async function signOut() {
+
+		// サインアウトする
+		const uid = await AuthService.signOut()
+
+		// 失敗
+		if (uid === null) {
+			return
+		}
+
+		// 成功
+		navigate("/")
+	}
 
 	return (
 
@@ -14,7 +31,7 @@ function AccountScreen() {
 				<p className="mt-4">メールアドレス</p>
 				<p className="text-slate-500">{AuthService.email()}</p>
 
-				<button onClick={() => AuthService.signOut()} className="mt-4 block mx-auto text-red-500 font-bold px-2 py-1 rounded-full hover:bg-red-100 dark:hover:bg-red-900 transition">サインアウト</button>
+				<button onClick={signOut} className="mt-4 block mx-auto text-red-500 font-bold px-2 py-1 rounded-full hover:bg-red-100 dark:hover:bg-red-900 transition">サインアウト</button>
 			</div>
 		</Modal>
 	);
