5d3e8397c5e6777b7aec7334bcbbdf4fbfd7a4b4
Yu357 <forestgreen14689@gmail.com>
2023-06-19 14:42:08 +0900

    feat: NotFoundScreenのUIを作成

--- src/views/screens/NotFoundScreen.tsx
+++ src/views/screens/NotFoundScreen.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom"
 import Header from "../components/sections/Header"
 
 interface Props {
@@ -12,10 +13,12 @@ function NotFoundScreen(props: Props) {
 
 			<Header />
 
-			<main>
+			<main className="mt-4 w-full mx-auto px-4 lg:width-lg lg:px-0">
 
-				<h1>Page not found</h1>
-				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non sapiente id architecto reiciendis. Porro, enim laborum! Veritatis, recusandae magni? Temporibus.</p>
+				<h1 className="text-2xl font-bold text-center">Page not found</h1>
+				<p className="mt-2 text-center">このページは存在しないか、現在のサインイン状態ではアクセスできません。</p>
+
+				<NavLink to="/" className="block mt-8 mx-auto w-fit font-bold py-2 px-8 bg-black text-white dark:bg-white dark:text-black rounded-full hover:opacity-70 transition">トップへ戻る</NavLink>
 			</main>
 		</div>
 	)
