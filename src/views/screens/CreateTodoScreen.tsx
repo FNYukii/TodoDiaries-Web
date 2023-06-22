db936ba50b08d13ca8861e060cf17abe6fb82274
Yu357 <forestgreen14689@gmail.com>
2023-06-22 10:17:50 +0900

    feat: 送信ボタンを配置

--- src/views/screens/CreateTodoScreen.tsx
+++ src/views/screens/CreateTodoScreen.tsx
@@ -1,22 +1,30 @@
 import { useState } from "react"
 import DynamicTextarea from "../components/inputs/DynamicTextarea"
 import Modal from "../components/others/Modal"
+import SubmitButton from "../components/buttons/SubmitButton"
 
 function CreateTodoScreen() {
 
 	document.title = "新規Todo - Todo Diaries"
 
-	const [text, setText] = useState("")
-
+	const [content, setContent] = useState("")
+	// eslint-disable-next-line 
+	const [isLoading, setIsLoading] = useState(false)
+	
 	return (
 
 		<Modal>
 
-			<div className="px-8 pb-7">
+			<div className="mx-8">
 
 				<h1 className="mt-4 text-2xl font-bold">新規Todo</h1>
 
-				<DynamicTextarea value={text} setValue={setText} placeholder="やること" className="mt-4 w-full py-2 bg-transparent border-b border-zinc-300 dark:border-zinc-600 focus:outline-none focus:border-blue-500 placeholder:text-zinc-400 dark:placeholder:text-zinc-600" />
+				<DynamicTextarea value={content} setValue={setContent} placeholder="やること" className="mt-4 w-full py-2 bg-transparent border-b border-zinc-300 dark:border-zinc-600 focus:outline-none focus:border-blue-500 placeholder:text-zinc-400 dark:placeholder:text-zinc-600" />
+			</div>
+
+			<div className="mt-4 mb-6 mr-4 ml-8 flex justify-end items-center">
+
+				<SubmitButton text="追加" isLoading={isLoading} disabled={content === ""} />
 			</div>
 		</Modal>
 	)
