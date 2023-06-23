295d0a8f19f62db56225ce77200f99308c8c64c9
Yu357 <forestgreen14689@gmail.com>
2023-06-23 10:24:45 +0900

    feat: iOSのトグル風にスタイルを整えた

--- src/views/components/inputs/ToggleButton.tsx
+++ src/views/components/inputs/ToggleButton.tsx
@@ -10,17 +10,25 @@ function ToggleButton() {
 
 			<p>isChecked: {isChecked ? "True" : "False"}</p>
 
-			<input
-				id="toggle"
-				type="checkbox"
-				checked={isChecked} onChange={() => setIsChecked(prevState => !prevState)}
-				className="hidden peer"
-			/>
 
-			<label
-				htmlFor="toggle"
-				className="block w-16 h-8 cursor-pointer bg-blue-500 appearance-none peer-checked:bg-green-500"
-			/>
+
+			<div className="relative">
+
+				<input
+					id="toggle"
+					type="checkbox"
+					checked={isChecked} onChange={() => setIsChecked(prevState => !prevState)}
+					className="hidden peer"
+				/>
+
+				<label
+					htmlFor="toggle"
+					className="block w-16 h-8 cursor-pointer bg-zinc-300 rounded-full appearance-none peer-checked:bg-green-500 transition"
+				/>
+
+				<div className="absolute top-0 bottom-0 my-auto left-1 peer-checked:left-9 w-6 h-6 rounded-full bg-white transition pointer-events-none	" />
+			</div>
+
 		</div>
 	);
 }
