329b10e63eb13a05a5db8ced2ad2905fd6aac97d
Yu357 <forestgreen14689@gmail.com>
2023-06-23 10:42:46 +0900

    clean: 不要なコードを削除

--- src/views/components/inputs/ToggleButton.tsx
+++ src/views/components/inputs/ToggleButton.tsx
@@ -6,29 +6,21 @@ function ToggleButton() {
 
 	return (
 
-		<div>
+		<div className="relative">
 
-			<p>isChecked: {isChecked ? "True" : "False"}</p>
+			<input
+				id="toggle"
+				type="checkbox"
+				checked={isChecked} onChange={() => setIsChecked(prevState => !prevState)}
+				className="hidden peer"
+			/>
 
+			<label
+				htmlFor="toggle"
+				className="block w-16 h-8 cursor-pointer bg-zinc-300 rounded-full appearance-none peer-checked:bg-green-500 transition"
+			/>
 
-
-			<div className="relative">
-
-				<input
-					id="toggle"
-					type="checkbox"
-					checked={isChecked} onChange={() => setIsChecked(prevState => !prevState)}
-					className="hidden peer"
-				/>
-
-				<label
-					htmlFor="toggle"
-					className="block w-16 h-8 cursor-pointer bg-zinc-300 rounded-full appearance-none peer-checked:bg-green-500 transition"
-				/>
-
-				<div className="absolute top-0 bottom-0 my-auto left-1 peer-checked:left-9 w-6 h-6 rounded-full bg-white transition pointer-events-none	" />
-			</div>
-
+			<div className="absolute top-0 bottom-0 my-auto left-1 peer-checked:left-9 w-6 h-6 rounded-full bg-white transition pointer-events-none	" />
 		</div>
 	);
 }
