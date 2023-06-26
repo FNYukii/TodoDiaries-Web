82b8f1d036d1889d43006621aad1a698b243de10
Yu357 <forestgreen14689@gmail.com>
2023-06-26 13:56:25 +0900

    feat: Todo作成画面にチェックボックスを配置

--- src/views/screens/CreateTodoScreen.tsx
+++ src/views/screens/CreateTodoScreen.tsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 import DynamicTextarea from "../components/inputs/DynamicTextarea"
 import Modal from "../components/others/Modal"
 import SubmitButton from "../components/buttons/SubmitButton"
-import ToggleButton from "../components/inputs/ToggleButton"
 
 function CreateTodoScreen() {
 
@@ -22,7 +21,20 @@ function CreateTodoScreen() {
 
 				<DynamicTextarea value={content} setValue={setContent} placeholder="やること" className="mt-4 w-full py-2 bg-transparent border-b border-zinc-300 dark:border-zinc-600 focus:outline-none focus:border-blue-500 placeholder:text-zinc-400 dark:placeholder:text-zinc-600" />
 
-				<ToggleButton />
+				<div className="mt-4 flex gap-8">
+
+					<label id="input-isPinned" className="w-fit cursor-pointer flex items-center gap-2">
+
+						<span>固定する</span>
+						<input id="input-isPinned" type="checkbox" className="w-4 h-4 cursor-pointer" />
+					</label>
+
+					<label id="input-isAchieved" className="w-fit cursor-pointer flex items-center gap-2">
+
+						<span>達成済み</span>
+						<input id="input-isAchieved" type="checkbox" className="w-4 h-4 cursor-pointer" />
+					</label>
+				</div>
 			</div>
 
 			<div className="mt-4 mb-7 mr-4 ml-8 flex justify-end items-center">
