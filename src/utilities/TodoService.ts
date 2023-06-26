a73f75e874f6f57130300030dd4ca8b01b0ba05f
Yu357 <forestgreen14689@gmail.com>
2023-06-26 15:10:41 +0900

    feat: readMaxOrderメソッドを作成

--- src/utilities/TodoService.ts
+++ src/utilities/TodoService.ts
@@ -1,20 +1,65 @@
-import { addDoc, collection, serverTimestamp } from "firebase/firestore"
+import { addDoc, collection, getDocs, limit, orderBy, query, serverTimestamp, where } from "firebase/firestore"
 import AuthService from "./AuthService"
 import { db } from "./firebase"
 
 class TodoService {
 
-	static async createTodo(content: string, isPinned: boolean, achievedAt: Date | null): Promise<string | null> {
+	static async readMaxOrder(isPinned: boolean): Promise<number | null> {
 
 		// サインインしていないなら終了　
-		const uid = AuthService.uid()
+		const uid = await AuthService.uid()
 
 		if (uid === null) {
 			return null
 		}
 
-		// TODO: orderの値を決定
-		const order = 100
+		// クエリを用意
+		const q = query(
+			collection(db, "comments"),
+			where("userId", "==", uid),
+			where("achievedAt", "==", null),
+			where("isPinned", "==", isPinned),
+			orderBy("order", "desc"),
+			limit(1)
+		)
+
+		try {
+
+			// サーバーorキャッシュから読み取り
+			const querySnapshot = await getDocs(q)
+
+			// 成功
+			// orderの最大値を取得する
+			const doc = querySnapshot.docs[0]
+			const order: number = doc.data().order ?? 0
+
+			return order
+
+		} catch (error) {
+
+			// 失敗
+			console.log(`Fail! Todo reading failed. ${error}`)
+			return null
+		}
+	}
+
+	static async createTodo(content: string, isPinned: boolean, achievedAt: Date | null): Promise<string | null> {
+
+		// UserIdを取得
+		const uid = await AuthService.uid()
+
+		// サインインしていないなら終了　
+		if (uid === null) {
+			return null
+		}
+
+		// orderの値を決定
+		const order = await this.readMaxOrder(isPinned)
+
+		// 新しいTodoのorderの値が決められないなら終了
+		if (order === null) {
+			return null
+		}
 
 		// Todoドキュメントを追加
 		try {
