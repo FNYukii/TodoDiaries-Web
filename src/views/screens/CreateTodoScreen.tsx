73cde7889186bbddfcaeaf2614bf81f91b5ac2af
Yu357 <forestgreen14689@gmail.com>
2023-06-26 14:09:17 +0900

    feat: マージン調整

--- src/views/screens/CreateTodoScreen.tsx
+++ src/views/screens/CreateTodoScreen.tsx
@@ -25,7 +25,7 @@ function CreateTodoScreen() {
 				<DynamicTextarea value={content} setValue={setContent} placeholder="やること" className="mt-4 w-full py-2 bg-transparent border-b border-zinc-300 dark:border-zinc-600 focus:outline-none focus:border-blue-500 placeholder:text-zinc-400 dark:placeholder:text-zinc-600" />
 			</div>
 
-			<div className="mt-4 ml-4 mr-8 flex gap-4">
+			<div className="mt-4 ml-4 mr-8 flex gap-2">
 
 				<label id="input-isPinned" className="w-fit py-1 px-4 cursor-pointer flex items-center gap-2 hover:bg-zinc-100 rounded-full transition">
 
