bcf4bbb107e34c1bc9b02c421a4af39d30c11d5d
Yu357 <forestgreen14689@gmail.com>
2023-07-13 10:36:21 +0900

    feat: マージン調整

--- src/views/screens/CreateTodoScreen.tsx
+++ src/views/screens/CreateTodoScreen.tsx
@@ -50,7 +50,7 @@ function CreateTodoScreen() {
 					<DynamicTextarea value={content} setValue={setContent} placeholder="やること" className="mt-4 w-full py-2 bg-transparent border-b border-zinc-300 dark:border-zinc-600 focus:outline-none focus:border-blue-500 placeholder:text-zinc-400 dark:placeholder:text-zinc-600" autoFocus />
 				</div>
 
-				<div className="mt-4 ml-4 mr-8 flex flex-wrap gap-2 items-center">
+				<div className="mt-4 ml-4 mr-8 flex flex-col gap-2">
 
 					<label id="input-isPinned" className="w-fit py-1 px-4 cursor-pointer flex items-center gap-2 hover:bg-zinc-100 dark:hover:bg-zinc-900 rounded-full transition">
 
@@ -65,7 +65,7 @@ function CreateTodoScreen() {
 					</label>
 
 					{isAchieved &&
-						<div className="ml-4 flex gap-2">
+						<div className="ml-4 mt-1 flex gap-2">
 							<span>達成日時</span>
 
 							<DatetimePicker date={achievedAt} setDate={setAchievedAt}/>
