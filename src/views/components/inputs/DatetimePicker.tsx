c2105e064e98fbff6b5927927dbb9ef3c2c14768
Yu357 <forestgreen14689@gmail.com>
2023-07-21 11:54:40 +0900

    feat: achievedAtのStateの値を更新できた

--- src/views/components/inputs/DatetimePicker.tsx
+++ src/views/components/inputs/DatetimePicker.tsx
@@ -34,8 +34,8 @@ function DatetimePicker(props: Props) {
 				<ThemeProvider theme={theme}>
 					<LocalizationProvider dateAdapter={AdapterDayjs}>
 
-						<MobileDatePicker value={dayjs(props.date)} slotProps={{ textField: { size: 'small' } }} />
-						<MobileTimePicker value={dayjs(props.date)} slotProps={{ textField: { size: 'small' } }} />
+						<MobileDatePicker value={dayjs(props.date)} onChange={(newValue) => props.setDate(newValue!.toDate())} slotProps={{ textField: { size: 'small' } }} />
+						<MobileTimePicker value={dayjs(props.date)} onChange={(newValue) => props.setDate(newValue!.toDate())} slotProps={{ textField: { size: 'small' } }} />
 					</LocalizationProvider>
 				</ThemeProvider>
 			</div>
