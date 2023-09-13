4f1d6fb840d9f472dcaf9e0e7d2a62c83f8884f5
Yu357 <forestgreen14689@gmail.com>
2023-09-13 15:17:14 +0900

    clean: TodoListからTodoRowを切り離し

--- src/views/components/lists/TodoList.tsx
+++ src/views/components/lists/TodoList.tsx
@@ -1,7 +1,5 @@
-import dayjs from "dayjs"
 import Todo from "../../../entities/Todo"
-import NavLinkToModal from "../others/NavLinkToModal"
-
+import TodoRow from "./TodoRow"
 
 interface Props {
 	todos: Todo[]
@@ -20,21 +18,9 @@ function TodoList(props: Props) {
 
 			<div className="mt-2 divide-y divide-zinc-200 dark:divide-zinc-700">
 
-				{props.todos.map((todo, index) => (
+				{props.todos.map((todo) => (
 
-					<NavLinkToModal to={`/todos/${todo.id}`} key={index} className="block py-3 px-4 flex gap-3 bg-white dark:bg-zinc-800 hover:bg-zinc-200 dark:hover:bg-zinc-700 transition first:rounded-t-xl last:rounded-b-xl">
-
-						<>
-							{todo.achievedAt !== null &&
-
-								<span className="text-zinc-500">
-									{dayjs(todo.achievedAt).format('HH:mm')}
-								</span>
-							}
-						</>
-
-						<p className="whitespace-pre-line">{todo.content}</p>
-					</NavLinkToModal>
+					<TodoRow todo={todo} />
 				))}
 			</div>
 		</div>
