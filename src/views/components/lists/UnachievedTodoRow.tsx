98689eb3cd50344bd851ebf99b05eefd8466f7da
Yu357 <forestgreen14689@gmail.com>
2023-09-13 19:46:51 +0900

    feating: ドラック可能なNavLinkをクリックすると、ページがリロードされる問題

--- src/views/components/lists/UnachievedTodoRow.tsx
+++ src/views/components/lists/UnachievedTodoRow.tsx
@@ -1,5 +1,6 @@
 import Todo from "../../../entities/Todo"
 import { useSortable } from "@dnd-kit/sortable"
+import NavLinkToModal from "../others/NavLinkToModal"
 // import { CSS } from "@dnd-kit/utilities"
 
 interface Props {
@@ -23,30 +24,27 @@ function UnachievedTodoRow(props: Props) {
 
 	return (
 
-		<div ref={setNodeRef} {...attributes} {...listeners} style={style} className="first:rounded-t-xl last:rounded-b-xl bg-white dark:bg-zinc-800 hover:bg-zinc-200 dark:hover:bg-zinc-700 transition">
+		<div
+			ref={setNodeRef}
+			{...attributes}
+			{...listeners}
+			style={style}
+			className="first:rounded-t-xl last:rounded-b-xl bg-white dark:bg-zinc-800 hover:bg-zinc-200 dark:hover:bg-zinc-700 transition"
+		>
 
-			{/* <NavLinkToModal to={`/todos/${props.todo.id}`} key={props.todo.id} className="block py-3 px-4 flex gap-3 ">
-
-				<>
-					{props.todo.achievedAt !== null &&
-
-						<span className="text-zinc-500">
-							{dayjs(props.todo.achievedAt).format('HH:mm')}
-						</span>
-					}
-				</>
+			<NavLinkToModal to={`/todos/${props.todo.id}`} className="block py-3 px-4 flex gap-3 ">
 
 				<p className="whitespace-pre-line">{props.todo.content}</p>
 
 				<p className="text-zinc-500">{props.todo.order}</p>
-			</NavLinkToModal> */}
+			</NavLinkToModal>
 
-			<div className="block py-3 px-4 flex gap-3">
+			{/* <div className="block py-3 px-4 flex gap-3">
 
 				<p className="whitespace-pre-line">{props.todo.content}</p>
 
 				<p className="text-zinc-500">{props.todo.order}</p>
-			</div>
+			</div> */}
 		</div>
 	)
 }
