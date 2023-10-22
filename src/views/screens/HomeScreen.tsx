238ebe77f06ed01d1073989c6ed67a4c6b4615b9
FNYukii <forestgreen14689@gmail.com>
2023-10-22 13:35:46 +0900

    feat: スマホサイズ用のボトムナビゲーションを実装

--- src/views/screens/HomeScreen.tsx
+++ src/views/screens/HomeScreen.tsx
@@ -2,10 +2,16 @@ import Header from "../components/sections/Header"
 import SecondColumn from "../components/columns/SecondColumn"
 import ThirdColumn from "../components/columns/ThirdColumn"
 import FirstColumn from "../components/columns/FirstColumn"
+import { useState } from "react"
+import { AiOutlineCheck } from "react-icons/ai"
+import { BsBarChartLine, BsCalendarCheck } from "react-icons/bs"
 
 function HomeScreen() {
 
 	document.title = "Todo Diaries"
+
+	// eslint-disable-next-line
+	const [tab, setTab] = useState(1)
 
 	return (
 
@@ -13,15 +19,30 @@ function HomeScreen() {
 
 			<Header showAccountButton />
 
-			<main className="w-full mx-auto px-4 lg:width-lg lg:px-0 overflow-y-auto scrollbar-none">
+			<div className="w-full mx-auto px-4 lg:width-lg lg:px-0 overflow-y-auto scrollbar-none">
 
 				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 h-full">
 
-					<FirstColumn className="pt-4 pb-8 pr-2 h-full overflow-y-auto scrollbar-styled" />
-					<SecondColumn className="pt-4 pb-8 pr-2 h-full overflow-y-auto scrollbar-styled hidden sm:block" />
-					<ThirdColumn className="pt-4 hidden lg:block" />
+					<FirstColumn className={`pt-4 pb-8 pr-2 h-full overflow-y-auto scrollbar-styled ${tab === 1 ? "block" : "hidden sm:block"}`} />
+					<SecondColumn className={`pt-4 pb-8 pr-2 h-full overflow-y-auto scrollbar-styled ${tab === 2 ? "block" : "hidden sm:block"}`} />
+					<ThirdColumn className={`pt-4 ${tab === 3 ? "block sm:hidden lg:block" : "hidden lg:block"}`} />
 				</div>
-			</main>
+			</div>
+
+			<div className="border-t px-4 py-4 flex justify-around gap-4">
+
+				<button onClick={() => setTab(1)}>
+					<AiOutlineCheck className={`text-2xl ${tab === 1 ? "text-blue-500" : "text-zinc-500"}`}/>
+				</button>
+
+				<button onClick={() => setTab(2)}>
+					<BsCalendarCheck className={`text-2xl ${tab === 2 ? "text-blue-500" : "text-zinc-500"}`}/>
+				</button>
+
+				<button onClick={() => setTab(3)}>
+					<BsBarChartLine className={`text-2xl ${tab === 3 ? "text-blue-500" : "text-zinc-500"}`}/>
+				</button>
+			</div>
 		</div>
 	)
 }
