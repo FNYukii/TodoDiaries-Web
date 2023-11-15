6cafae0db8f58af428717c4763a5ae2de58249fa
FNYukii <forestgreen14689@gmail.com>
2023-11-15 17:07:47 +0900

    feat: カラム3のスクロールバーを改善

--- src/views/screens/HomeScreen.tsx
+++ src/views/screens/HomeScreen.tsx
@@ -19,11 +19,11 @@ function HomeScreen() {
 
 			<main className="w-full mx-auto px-4 lg:w-[1024px] lg:px-0   overflow-y-hidden">
 
-				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 h-full   overflow-x-visible">
+				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 h-full">
 
-					<FirstColumn className={`pt-4 pb-8 pr-2 scrollbar-styled overflow-y-auto ${tab === 1 ? "block" : "hidden sm:block"}`} />
-					<SecondColumn className={`pt-4 pb-7 pr-2 scrollbar-styled overflow-y-auto ${tab === 2 ? "block" : "hidden sm:block"}`} />
-					<ThirdColumn className={`pt-4 pb-8 pr-2 mr-[-1rem] scrollbar-styled overflow-y-auto overflow-x-visible ${tab === 3 ? "block sm:hidden lg:block" : "hidden lg:block"}`} />
+					<FirstColumn className={`scrollbar-styled overflow-y-auto   pt-4 pb-8 pr-2   ${tab === 1 ? "block" : "hidden sm:block"}`} />
+					<SecondColumn className={`scrollbar-styled overflow-y-auto   pt-4 pb-7 pr-2 ml-4    ${tab === 2 ? "block" : "hidden sm:block"}`} />
+					<ThirdColumn className={`scrollbar-styled overflow-y-auto   pt-4 pb-8 pl-2   scrollbar-left   ${tab === 3 ? "block sm:hidden lg:block" : "hidden lg:block"}`} />
 				</div>
 			</main>
 
